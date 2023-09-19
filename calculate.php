628c3e65b2031241746fc3628e80514f213949f1
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-09-19 14:32:31 +0200

    validate form

--- calculate.php
+++ calculate.php
@@ -1,14 +1,23 @@
 <?php
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+$totalprice = 0;
+$price = 0;
+$numberproducts = 0;
+function calculatePrice(&$totalprice, &$price, &$numberproducts) {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ( isset($_POST['price']) && isset($_POST['numberproducts'])) {
         $price = $_POST['price'];
         $numberproducts = $_POST['numberproducts'];
+        }
     }
+
+    $iva = 0.21;
+    $ivaprice = $iva * $price;
+    $oneprice = $price - $ivaprice;
+    $totalprice = $numberproducts * $oneprice;
+    return $totalprice;
 }
 
-print_r($price);
-print_r($numberproducts);
+echo calculatePrice($totalprice, $price, $numberproducts);
 
 ?>
 <!DOCTYPE html>
@@ -17,8 +26,25 @@ print_r($numberproducts);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculate product</title>
+    <link rel="stylesheet" href="./calculate.css">
 </head>
 <body>
-    
+<div class="calculate">
+    <div class="calculate_header">
+        <a href="./product.php">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" style="fill: blue;">
+                <path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z"/>
+            </svg>
+        </a>
+    </div>
+    <h2 class="calculate_subtitle">Precio total: <?php echo $totalprice?> €</h2>
+    <form method="post" action='calculate.php' class="calculate_form">
+        <div class="calculate_container">
+            <label class="calculate--label" for="money">Introduce cantidad a pagar: </label>
+            <input class="calculate--input" type="number" name="money" id="money" required />
+        </div>
+        <input class="calculate--btn" type="submit" value="CALCULAR CANTIDAD A DEVOLVER" />
+        </form>
+    </div>
 </body>
 </html>
--- product.php
+++ product.php
@@ -10,7 +10,7 @@
 <body>
     <div class="product">
         <h1 class="product_title">Calcular precio:</h1>
-        <form method="post" action='calculate.php' class="product_form">
+        <form method="post" action='calculate.php' class="product_form" onsubmit="return validateForm()">
             <div class="product_container">
                 <label class="product--label" for="price">Precio del artículo: </label>
                 <input class="product--input" type="number" name="price" id="price" required />
@@ -21,6 +21,21 @@
             </div>
             <input class="product--btn" type="submit" value="CALCULAR" />
         </form>
+        <div id="error-message" style="color: red;"></div>
     </div>
+    <script>
+        const validateForm = () => {
+            const price = document.getElementById('price').value;
+            const numberProducts = document.getElementById('numberproducts').value;
+            const errorMessage = document.getElementById('error-message');
+            if ( price <= 0 || numberProducts <= 0) {
+                errorMessage.textContent = 'Los números no pueden ser negativos o igual a 0.'
+                return false;
+            } else {
+                errorMessage.textContent = '';
+                return true;
+            }
+        }
+    </script>
 </body>
 </html>
